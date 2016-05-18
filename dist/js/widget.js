919a0e59e64c316a81aacb29f3a41b6d7d874373
Stuart Lees <stuartlees@gmail.com>
2016-05-18 17:37:25 -0400

    Revised and turned back on integration tests [stage-2]

--- dist/js/widget.js
+++ dist/js/widget.js
@@ -847,7 +847,7 @@ RiseVision.Video = (function (window, gadgets) {
     // Ensures loading messaging is hidden and video gets shown
     _message.hide();
 
-    if (!_viewerPaused) {
+    if (!_viewerPaused && _player) {
       _player.play();
     }
   }
