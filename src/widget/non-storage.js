e393a5a6a3c6e349a80a04b16db08de1e104c266
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-25 14:25:10 -0400

    Fix linting issues in widget/non-storage [stage-1]

--- src/widget/non-storage.js
+++ src/widget/non-storage.js
@@ -1,69 +1,66 @@
 var RiseVision = RiseVision || {};
 RiseVision.Video = RiseVision.Video || {};
 
-RiseVision.Video.NonStorage = function (data) {
+RiseVision.Video.NonStorage = function( data ) {
   "use strict";
 
   var riseCache = RiseVision.Common.RiseCache,
-    utils = RiseVision.Common.Utilities;
+    utils = RiseVision.Common.Utilities,
+    // 15 minutes
+    _refreshDuration = 900000,
+    _refreshIntervalId = null,
+    _isLoading = true,
+    _url = "";
 
-  var _refreshDuration = 900000,  // 15 minutes
-    _refreshIntervalId = null;
-
-  var _isLoading = true;
-
-  var _url = "";
-
-  function _getFile(omitCacheBuster) {
-    riseCache.getFile(_url, function (response, error) {
+  function _getFile( omitCacheBuster ) {
+    riseCache.getFile( _url, function( response, error ) {
       var statusCode = 0,
         errorMessage;
 
-      if (!error) {
+      if ( !error ) {
 
-        if (_isLoading) {
+        if ( _isLoading ) {
           _isLoading = false;
 
-          RiseVision.Video.onFileInit(response.url);
+          RiseVision.Video.onFileInit( response.url );
 
           // start the refresh interval
           _startRefreshInterval();
 
         } else {
-          RiseVision.Video.onFileRefresh(response.url);
+          RiseVision.Video.onFileRefresh( response.url );
         }
 
       } else {
         // error occurred
-        RiseVision.Video.logEvent({
+        RiseVision.Video.logEvent( {
           "event": "non-storage error",
           "event_details": error.message,
           "file_url": response.url
-        }, true);
+        }, true );
 
-        if (riseCache.isV2Running()) {
-          errorMessage = riseCache.getErrorMessage(statusCode);
-        }
-        else {
+        if ( riseCache.isV2Running() ) {
+          errorMessage = riseCache.getErrorMessage( statusCode );
+        } else {
           // Show a different message if there is a 404 coming from rise cache
-          if(error.message){
-            statusCode = +error.message.substring(error.message.indexOf(":")+2);
+          if ( error.message ) {
+            statusCode = +error.message.substring( error.message.indexOf( ":" ) + 2 );
           }
 
-          errorMessage = utils.getRiseCacheErrorMessage(statusCode);
+          errorMessage = utils.getRiseCacheErrorMessage( statusCode );
         }
 
         // show the error
-        RiseVision.Video.showError(errorMessage);
+        RiseVision.Video.showError( errorMessage );
       }
-    }, omitCacheBuster);
+    }, omitCacheBuster );
   }
 
   function _startRefreshInterval() {
-    if (_refreshIntervalId === null) {
-      _refreshIntervalId = setInterval(function () {
-        _getFile(false);
-      }, _refreshDuration);
+    if ( _refreshIntervalId === null ) {
+      _refreshIntervalId = setInterval( function() {
+        _getFile( false );
+      }, _refreshDuration );
     }
   }
 
@@ -72,11 +69,11 @@ RiseVision.Video.NonStorage = function (data) {
    */
   function init() {
     // Handle pre-merge use of "url" setting property
-    _url = (data.url && data.url !== "") ? data.url : data.selector.url;
+    _url = ( data.url && data.url !== "" ) ? data.url : data.selector.url;
 
-    _url = utils.addProtocol(_url);
+    _url = utils.addProtocol( _url );
 
-    _getFile(true);
+    _getFile( true );
   }
 
   return {
