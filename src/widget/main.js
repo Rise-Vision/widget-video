8399cb0fe0fafa14edfbc6b8fca091bbb53f56de
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-25 14:24:36 -0400

    Fix linting issues in widget/main.js [stage-1]

--- src/widget/main.js
+++ src/widget/main.js
@@ -1,45 +1,44 @@
-/* global gadgets, RiseVision, config */
+/* global gadgets, RiseVision, config, version */
 
-(function (window, gadgets) {
+( function( window, gadgets ) {
   "use strict";
 
   var prefs = new gadgets.Prefs(),
-    id = prefs.getString("id");
+    id = prefs.getString( "id" );
 
   // Disable context menu (right click menu)
-  window.oncontextmenu = function () {
+  window.oncontextmenu = function() {
     return false;
   };
 
-  function configure(names, values) {
+  function configure( names, values ) {
     var additionalParams = null,
       mode = "",
       companyId = "",
       displayId = "";
 
-    if (Array.isArray(names) && names.length > 0 && Array.isArray(values) && values.length > 0) {
-      if (names[0] === "companyId") {
-        companyId = values[0];
+    if ( Array.isArray( names ) && names.length > 0 && Array.isArray( values ) && values.length > 0 ) {
+      if ( names[ 0 ] === "companyId" ) {
+        companyId = values[ 0 ];
       }
 
-      if (names[1] === "displayId") {
-        if (values[1]) {
-          displayId = values[1];
-        }
-        else {
+      if ( names[ 1 ] === "displayId" ) {
+        if ( values[ 1 ] ) {
+          displayId = values[ 1 ];
+        } else {
           displayId = "preview";
         }
       }
 
-      RiseVision.Common.LoggerUtils.setIds(companyId, displayId);
-      RiseVision.Common.LoggerUtils.setVersion(version);
+      RiseVision.Common.LoggerUtils.setIds( companyId, displayId );
+      RiseVision.Common.LoggerUtils.setVersion( version );
 
-      if (names[2] === "additionalParams") {
-        additionalParams = JSON.parse(values[2]);
+      if ( names[ 2 ] === "additionalParams" ) {
+        additionalParams = JSON.parse( values[ 2 ] );
 
-        if (Object.keys(additionalParams.storage).length !== 0) {
+        if ( Object.keys( additionalParams.storage ).length !== 0 ) {
           // storage file or folder selected
-          if (!additionalParams.storage.fileName) {
+          if ( !additionalParams.storage.fileName ) {
             // folder was selected
             mode = "folder";
           } else {
@@ -51,7 +50,7 @@
           mode = "file";
         }
 
-        RiseVision.Video.setAdditionalParams(additionalParams, mode);
+        RiseVision.Video.setAdditionalParams( additionalParams, mode );
       }
     }
   }
@@ -69,70 +68,70 @@
   }
 
   function init() {
-    if (id && id !== "") {
-      gadgets.rpc.register("rscmd_play_" + id, play);
-      gadgets.rpc.register("rscmd_pause_" + id, pause);
-      gadgets.rpc.register("rscmd_stop_" + id, stop);
+    if ( id && id !== "" ) {
+      gadgets.rpc.register( "rscmd_play_" + id, play );
+      gadgets.rpc.register( "rscmd_pause_" + id, pause );
+      gadgets.rpc.register( "rscmd_stop_" + id, stop );
 
-      gadgets.rpc.register("rsparam_set_" + id, configure);
-      gadgets.rpc.call("", "rsparam_get", null, id, ["companyId", "displayId", "additionalParams"]);
+      gadgets.rpc.register( "rsparam_set_" + id, configure );
+      gadgets.rpc.call( "", "rsparam_get", null, id, [ "companyId", "displayId", "additionalParams" ] );
     }
   }
 
   // check which version of Rise Cache is running and dynamically add rise-storage dependencies
-  RiseVision.Common.RiseCache.isV2Running(function (isV2) {
+  RiseVision.Common.RiseCache.isV2Running( function( isV2 ) {
     var fragment = document.createDocumentFragment(),
-      link = document.createElement("link"),
-      webcomponents = document.createElement("script"),
-      href = config.COMPONENTS_PATH + ((isV2) ? "rise-storage-v2" : "rise-storage") + "/rise-storage.html",
-      storage = document.createElement("rise-storage"),
+      link = document.createElement( "link" ),
+      webcomponents = document.createElement( "script" ),
+      href = config.COMPONENTS_PATH + ( ( isV2 ) ? "rise-storage-v2" : "rise-storage" ) + "/rise-storage.html",
+      storage = document.createElement( "rise-storage" ),
       storageReady = false,
       polymerReady = false;
 
     function onPolymerReady() {
-      window.removeEventListener("WebComponentsReady", onPolymerReady);
+      window.removeEventListener( "WebComponentsReady", onPolymerReady );
       polymerReady = true;
 
-      if (storageReady && polymerReady) {
+      if ( storageReady && polymerReady ) {
         init();
       }
     }
 
     function onStorageReady() {
-      storage.removeEventListener("rise-storage-ready", onStorageReady);
+      storage.removeEventListener( "rise-storage-ready", onStorageReady );
       storageReady = true;
 
-      if (storageReady && polymerReady) {
+      if ( storageReady && polymerReady ) {
         init();
       }
     }
 
     webcomponents.src = config.COMPONENTS_PATH + "webcomponentsjs/webcomponents-lite.min.js";
-    window.addEventListener("WebComponentsReady", onPolymerReady);
+    window.addEventListener( "WebComponentsReady", onPolymerReady );
 
     // add the webcomponents polyfill source to the document head
-    document.getElementsByTagName("head")[0].appendChild(webcomponents);
+    document.getElementsByTagName( "head" )[ 0 ].appendChild( webcomponents );
 
-    link.setAttribute("rel", "import");
-    link.setAttribute("href", href);
+    link.setAttribute( "rel", "import" );
+    link.setAttribute( "href", href );
 
     // add the rise-storage <link> element to document head
-    document.getElementsByTagName("head")[0].appendChild(link);
+    document.getElementsByTagName( "head" )[ 0 ].appendChild( link );
 
-    storage.setAttribute("id", "videoStorage");
-    storage.setAttribute("refresh", 5);
+    storage.setAttribute( "id", "videoStorage" );
+    storage.setAttribute( "refresh", 5 );
 
-    if (isV2) {
-      storage.setAttribute("usage", "widget");
+    if ( isV2 ) {
+      storage.setAttribute( "usage", "widget" );
     }
 
-    storage.addEventListener("rise-storage-ready", onStorageReady);
-    fragment.appendChild(storage);
+    storage.addEventListener( "rise-storage-ready", onStorageReady );
+    fragment.appendChild( storage );
 
     // add the <rise-storage> element to the body
-    document.body.appendChild(fragment);
-  });
+    document.body.appendChild( fragment );
+  } );
 
-})(window, gadgets);
+} )( window, gadgets );
 
 
