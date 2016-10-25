9d30e5c287bfa0131ecde1751d09ead74ad5d966
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-25 10:24:18 -0400

    Add ESLint dependencies and configuration file [stage-1]

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "idiomatic",
+  "rules": {
+    "quotes": [
+      "error",
+      "double"
+    ]
+  }
+};
