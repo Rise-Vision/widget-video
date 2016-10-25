3433c89a916958ef46568e35c70751a60cd2b46f
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-25 14:00:29 -0400

    Fix linting issues in config files [stage-1]

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,11 +1,11 @@
 /* exported config */
-if (typeof angular !== "undefined") {
-  angular.module("risevision.common.i18n.config", [])
-    .constant("LOCALES_PREFIX", "locales/translation_")
-    .constant("LOCALES_SUFIX", ".json");
+if ( typeof angular !== "undefined" ) {
+  angular.module( "risevision.common.i18n.config", [] )
+    .constant( "LOCALES_PREFIX", "locales/translation_" )
+    .constant( "LOCALES_SUFIX", ".json" );
 }
 
-var  config = {
+const config = {
   STORAGE_ENV: "prod",
   COMPONENTS_PATH: "components/"
 };
