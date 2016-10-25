30ca0ceb923871ecf7d0bdf1111127f43cf71d1d
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-25 14:25:26 -0400

    Fix linting issues in widget/player-utils.js [stage-1]

--- src/widget/player-utils.js
+++ src/widget/player-utils.js
@@ -1,36 +1,35 @@
 var RiseVision = RiseVision || {};
 RiseVision.Video = RiseVision.Video || {};
 
-RiseVision.Video.PlayerUtils = (function () {
+RiseVision.Video.PlayerUtils = ( function() {
   "use strict";
 
   /*
    *  Public  Methods
    */
-  function getPlaylist (list) {
-    /*jshint validthis:true */
-    
-    var playlist = [];
+  function getPlaylist( list ) {
+    var i,
+      playlist = [];
 
-    for (var i = 0; i < list.length; i += 1) {
-      playlist.push({
-        file: list[i],
-        type: this.getVideoFileType(list[i])
-      });
+    for ( i = 0; i < list.length; i += 1 ) {
+      playlist.push( {
+        file: list[ i ],
+        type: this.getVideoFileType( list[ i ] )
+      } );
     }
 
     return playlist;
   }
-  
-  function getVideoFileType (url) {
-    var extensions = [".mp4", ".webm"],
+
+  function getVideoFileType( url ) {
+    var extensions = [ ".mp4", ".webm" ],
       urlLowercase = url.toLowerCase(),
       type = null,
       i;
 
-    for (i = 0; i <= extensions.length; i += 1) {
-      if (urlLowercase.indexOf(extensions[i]) !== -1) {
-        type = extensions[i].substr(extensions[i].lastIndexOf(".") + 1);
+    for ( i = 0; i <= extensions.length; i += 1 ) {
+      if ( urlLowercase.indexOf( extensions[ i ] ) !== -1 ) {
+        type = extensions[ i ].substr( extensions[ i ].lastIndexOf( "." ) + 1 );
         break;
       }
     }
@@ -43,4 +42,4 @@ RiseVision.Video.PlayerUtils = (function () {
     "getVideoFileType": getVideoFileType
   };
 
-})();
+} )();
