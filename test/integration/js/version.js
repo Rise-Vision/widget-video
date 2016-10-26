8e10c7e396f6d211c8ace0424d20cdc2d94ae444
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-26 09:01:40 -0400

    Fix linting issues in integration tests [stage-1]

--- test/integration/js/version.js
+++ test/integration/js/version.js
@@ -1,40 +1,42 @@
-var isV2Running = false,
-  xhr, requests;
+/* global suiteSetup, suiteTeardown, test, assert, RiseVision, sinon, config */
 
-sinon.stub(RiseVision.Common.RiseCache, "isV2Running", function (callback) {
+var xhr,
+  requests,
+  isV2Running = false;
+
+sinon.stub( RiseVision.Common.RiseCache, "isV2Running", function( callback ) {
   xhr = sinon.useFakeXMLHttpRequest();
   requests = [];
 
-  xhr.onCreate = function (xhr) {
-    requests.push(xhr);
+  xhr.onCreate = function( xhr ) {
+    requests.push( xhr );
   };
 
-  sinon.stub(RiseVision.Video, "setAdditionalParams");
+  sinon.stub( RiseVision.Video, "setAdditionalParams" );
 
   RiseVision.Common.RiseCache.isV2Running.restore();
-  RiseVision.Common.RiseCache.isV2Running(callback);
-});
+  RiseVision.Common.RiseCache.isV2Running( callback );
+} );
 
-suiteSetup(function () {
-  if (!isV2Running) {
-    requests[0].respond(200);
+suiteSetup( function() {
+  if ( !isV2Running ) {
+    requests[ 0 ].respond( 200 );
+  } else {
+    requests[ 0 ].respond( 404 );
+    requests[ 1 ].respond( 200 );
   }
-  else {
-    requests[0].respond(404);
-    requests[1].respond(200);
-  }
-});
+} );
 
-suiteTeardown(function() {
+suiteTeardown( function() {
   xhr.restore();
   RiseVision.Video.setAdditionalParams.restore();
-});
+} );
 
-test("rise-storage element should be added to body", function() {
-  assert.isNotNull(document.querySelector("rise-storage"));
-});
+test( "rise-storage element should be added to body", function() {
+  assert.isNotNull( document.querySelector( "rise-storage" ) );
+} );
 
-test("polyfill added to document head", function () {
-  var head = document.getElementsByTagName("head")[0];
-  assert.isNotNull(head.querySelector("script[src='" + config.COMPONENTS_PATH + "webcomponentsjs/webcomponents-lite.min.js'"));
-});
+test( "polyfill added to document head", function() {
+  var head = document.getElementsByTagName( "head" )[ 0 ];
+  assert.isNotNull( head.querySelector( "script[src='" + config.COMPONENTS_PATH + "webcomponentsjs/webcomponents-lite.min.js'" ) );
+} );
