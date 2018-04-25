947d53e72f0e6331d4d7930a1083db265af346d1
Rudi Bravo <rudibravo@gmail.com>
2018-04-25 13:51:53 -0300

    disable unit test [stage-2]

--- test/unit/widget/player-vjs-spec.js
+++ test/unit/widget/player-vjs-spec.js
@@ -1,96 +1,96 @@
-// /* global describe, before, after, it, expect, sinon, RiseVision */
-// /* exported videojs */
+/* global describe, before, after, it, expect, sinon, RiseVision */
+/* exported videojs */
 
-// /* eslint-disable func-names */
+/* eslint-disable func-names */
 
-// "use strict";
+"use strict";
 
-// var loadSpinnerComponent,
-//   videoJSObj =
-//   {
-//     src: function() {},
-//     options: function() {},
-//     pause: function() {},
-//     paused: function() {},
-//     play: function() {},
-//     remainingTime: function() {},
-//     currentTime: function() {},
-//     on: function() {},
-//     getChild: function() {
-//       return loadSpinnerComponent
-//     },
-//     removeChild: function() {}
-//   },
-//   videojs = function( tag, opts, cb ) {
-//     videoJSObj.options( opts );
+var loadSpinnerComponent,
+  videoJSObj =
+  {
+    src: function() {},
+    options: function() {},
+    pause: function() {},
+    paused: function() {},
+    play: function() {},
+    remainingTime: function() {},
+    currentTime: function() {},
+    on: function() {},
+    getChild: function() {
+      return loadSpinnerComponent
+    },
+    removeChild: function() {}
+  },
+  videojs = function( tag, opts, cb ) {
+    videoJSObj.options( opts );
 
-//     // execute the callback after returning instance
-//     setTimeout( function() {
-//       cb();
-//     }, 20 );
+    // execute the callback after returning instance
+    setTimeout( function() {
+      cb();
+    }, 20 );
 
-//     return videoJSObj;
-//   };
+    return videoJSObj;
+  };
 
-// describe( "init()", function() {
-//   var params =
-//     {
-//       "video": {
-//         "scaleToFit": true,
-//         "volume": 50,
-//         "controls": true,
-//         "autoplay": true,
-//         "resume": true,
-//         "pause": 5
-//       },
-//       "width": 1200,
-//       "height": 800
-//     },
-//     files = [
-//       "https://test.com/test%2Fvideos%2Fvideo1.webm"
-//     ],
-//     optionsSpy,
-//     getChildSpy,
-//     removeChildSpy,
-//     srcSpy;
+describe( "init()", function() {
+  var params =
+    {
+      "video": {
+        "scaleToFit": true,
+        "volume": 50,
+        "controls": true,
+        "autoplay": true,
+        "resume": true,
+        "pause": 5
+      },
+      "width": 1200,
+      "height": 800
+    },
+    files = [
+      "https://test.com/test%2Fvideos%2Fvideo1.webm"
+    ],
+    optionsSpy,
+    getChildSpy,
+    removeChildSpy,
+    srcSpy;
 
-//   before( function() {
-//     optionsSpy = sinon.spy( videoJSObj, "options" );
-//     getChildSpy = sinon.spy( videoJSObj, "getChild" );
-//     removeChildSpy = sinon.spy( videoJSObj, "removeChild" );
-//     srcSpy = sinon.spy( videoJSObj, "src" );
-//   } );
+  before( function() {
+    optionsSpy = sinon.spy( videoJSObj, "options" );
+    getChildSpy = sinon.spy( videoJSObj, "getChild" );
+    removeChildSpy = sinon.spy( videoJSObj, "removeChild" );
+    srcSpy = sinon.spy( videoJSObj, "src" );
+  } );
 
-//   after( function() {
-//     videoJSObj.options.restore();
-//     videoJSObj.src.restore();
-//   } );
+  after( function() {
+    videoJSObj.options.restore();
+    videoJSObj.src.restore();
+  } );
 
-//   it( "should setup videojs with correct options and src value", function() {
-//     var player = new RiseVision.Video.PlayerVJS( params );
+  it( "should setup videojs with correct options and src value", function() {
+    var player = new RiseVision.Video.PlayerVJS( params );
 
-//     player.init( files );
+    player.init( files );
 
-//     expect( optionsSpy ).to.have.been.calledWith( {
-//       controls: true,
-//       fluid: false,
-//       height: params.height,
-//       width: params.width
-//     } );
+    expect( optionsSpy ).to.have.been.calledWith( {
+      controls: true,
+      fluid: false,
+      height: params.height,
+      width: params.width
+    } );
 
-//     expect( getChildSpy ).to.have.been.calledWith( "loadingSpinner" );
+    expect( getChildSpy ).to.have.been.calledWith( "loadingSpinner" );
 
-//     expect( removeChildSpy ).to.have.been.calledWith( loadSpinnerComponent );
+    expect( removeChildSpy ).to.have.been.calledWith( loadSpinnerComponent );
 
-//     // delay for callback execution
-//     setTimeout( function() {
-//       expect( srcSpy ).to.have.been.calledWith( {
-//         type: "video/webm",
-//         src: "https://test.com/test%2Fvideos%2Fvideo1.webm"
-//       } );
-//     }, 30 );
+    // delay for callback execution
+    setTimeout( function() {
+      expect( srcSpy ).to.have.been.calledWith( {
+        type: "video/webm",
+        src: "https://test.com/test%2Fvideos%2Fvideo1.webm"
+      } );
+    }, 30 );
 
 
-//   } );
+  } );
 
-// } );
+} );
