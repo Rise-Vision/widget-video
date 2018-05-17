1f5bff0bf1bc5dcb589a02b3b7607efb0515b245
Jaime Yoon <6478127+jungeunyoon@users.noreply.github.com>
2018-05-17 15:56:10 +0900

    removes references to deprecated rv-common-1l8n

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,7 +1,7 @@
 /* exported config */
 if ( typeof angular !== "undefined" ) {
   angular.module( "risevision.common.i18n.config", [] )
-    .constant( "LOCALES_PREFIX", "components/rv-common-i18n/dist/locales/translation_" )
+    .constant( "LOCALES_PREFIX", "components/common-header/dist/locales/translation_" )
     .constant( "LOCALES_SUFIX", ".json" );
 
   angular.module( "risevision.widget.common.storage-selector.config" )
